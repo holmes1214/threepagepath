1b361a44a1b64249cb61f959c405069643394028
howard@egenie.cn <howard@egenie.cn>
2016-09-10 18:12:27 +0800

    push

---
+++ src/main/java/com/zuora/homework/threepagepath/HomeWork.java
@@ -0,0 +1,16 @@
+package com.zuora.homework.threepagepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages="com.zuora")
+@EnableScheduling
+public class HomeWork {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(HomeWork.class);
+        application.setWebEnvironment(false);
+        application.run(args);
+	}
+}
